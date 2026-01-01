d18786f0b41b736b90690f6986c45eae3a8ff822
Johannes Schmidt <johannes.schmidt42@gmail.com>
2026-01-01 20:03:54 +0100

    v3.3.8: Fix console errors on page load

--- Ai.Tlbx.MiddleManager.ConHost/Program.cs
+++ Ai.Tlbx.MiddleManager.ConHost/Program.cs
@@ -8,7 +8,7 @@ namespace Ai.Tlbx.MiddleManager.ConHost;
 
 public static class Program
 {
-    public const string Version = "3.3.7";
+    public const string Version = "3.3.8";
 
     private static readonly string LogDir = Path.Combine(
         Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData),
